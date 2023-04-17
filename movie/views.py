adbf66f3ddee4383252f80d1be72d5fcb7bb46fd
Dastan-Oskonbaev <dastiw1910@gmail.com>
2023-04-17 16:21:53 +0600

    Lesson 18

--- movie/views.py
+++ movie/views.py
@@ -1,20 +1,29 @@
-from django.http import request
+from django.db.models import Q
+from django.http import request, JsonResponse
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 import movie
-from .forms import  ReviewForm
-from .models import Movie, Category, Actor
+from .forms import ReviewForm
+from .models import Movie, Category, Actor, Genre
 
 
-class MovieView(ListView):
+class GenreYear:
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_year(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MovieView(GenreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     # template_name = "movie_movies.html"
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     model = Movie
     slug_field = "url"
 
@@ -32,7 +41,31 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     model = Actor
     template_name = 'movie/actor.html'
     slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear, ListView):
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        return queryset
+
+
+class JsonFilterMoviesView(ListView):
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        ).distinct().value('title', 'tagline', 'url', 'poster')
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        queryset= list(self.get_queryset())
+        return JsonResponse({'movies': queryset}, safe=False)
+
